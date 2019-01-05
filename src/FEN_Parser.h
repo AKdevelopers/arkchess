dbc9ae7e5752361071d71251a1651a6523185d8b
Atharva Karandikar <ak.karandikar@mail.utoronto.ca>
2019-01-05 12:05:22 -0500

    Added new functions for parsing

--- src/FEN_Parser.h
+++ src/FEN_Parser.h
@@ -3,3 +3,7 @@ struct Board ParseFEN(char *fen_str);
 struct Board GetBoardArray(char *placement);
 struct Board ParseBoard(char* board_tokens[]);
 struct Board PopulateBitBoard(char board[8][8], struct Board state);
+
+void init_position(struct Board *pos, char *fen_str);
+char parse_castling_rights(char *castling_rights);
+
