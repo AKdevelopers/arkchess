532d5e2407c1cdda2815d468525ead0b30ea568f
Atharva Karandikar <ak.karandikar@mail.utoronto.ca>
2019-01-05 10:49:37 -0500

    Created new types.c file and moved all constant and type definitions to it

--- src/ark.h
+++ src/ark.h
@@ -1,40 +1,6 @@
 #ifndef ARK_INCLUDED
 #define ARK_INCLUDED
 #include "types.h"
-struct Board {
-    // White pieces
-    U64 white_pawns;
-    U64 white_rooks;
-    U64 white_bishops;
-    U64 white_knights;
-    U64 white_queen;
-    U64 white_king;
-
-    // Black pieces
-    U64 black_pawns;
-    U64 black_rooks;
-    U64 black_bishops;
-    U64 black_knights;
-    U64 black_queen;
-    U64 black_king;
-
-    // Colors
-    U64 all_white_pieces;
-    U64 all_black_pieces;
-
-    // All pieces
-    U64 all_pieces;
-
-    int colour_to_move;
-};
-
-extern U64 ClearFile[8];
-extern U64 MaskFile[8];
-extern U64 ClearRank[8];
-extern U64 MaskRank[8];
-extern U64 DiagonalMask[15];
-extern U64 AntiDiagonalMask[15];
-
 int GetIndex(int rank, int file); 
 struct Board InitPosition(char* FEN_str);
 U64 KingMoves(U64 king_loc, U64 all_pieces);
--- src/types.h
+++ src/types.h
@@ -3,5 +3,48 @@
 #include <stdint.h>
 typedef uint64_t U64;
 typedef uint16_t U16;
+
+struct Board {
+    // White pieces
+    U64 white_pawns;
+    U64 white_rooks;
+    U64 white_bishops;
+    U64 white_knights;
+    U64 white_queen;
+    U64 white_king;
+
+    // Black pieces
+    U64 black_pawns;
+    U64 black_rooks;
+    U64 black_bishops;
+    U64 black_knights;
+    U64 black_queen;
+    U64 black_king;
+
+    // Colors
+    U64 all_white_pieces;
+    U64 all_black_pieces;
+
+    // All pieces
+    U64 all_pieces;
+
+    int colour_to_move;
+	char castling_rights;
+	int ep_square; // 0..63
+	int half_move_ctr;
+	int full_move_ctr;
+};
+
+extern U64 ClearFile[8];
+extern U64 MaskFile[8];
+extern U64 ClearRank[8];
+extern U64 MaskRank[8];
+extern U64 DiagonalMask[15];
+extern U64 AntiDiagonalMask[15];
+
+extern const char KINGSIDE_WHITE;
+extern const char QUEENSIDE_WHITE;
+extern const char KINGSIDE_BLACK;
+extern const char QUEENSIDE_BLACK;
 #endif 
 
