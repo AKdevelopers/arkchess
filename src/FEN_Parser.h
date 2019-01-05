0cc5126330a34b5977a89625ce4d5466ef74b73e
Atharva Karandikar <ak.karandikar@mail.utoronto.ca>
2019-01-05 15:20:38 -0500

    Revamped parser

--- src/FEN_Parser.h
+++ src/FEN_Parser.h
@@ -1,9 +1,3 @@
 int GetLEIndex(int rank, int file);
-struct Board ParseFEN(char *fen_str);
-struct Board GetBoardArray(char *placement);
-struct Board ParseBoard(char* board_tokens[]);
-struct Board PopulateBitBoard(char board[8][8], struct Board state);
-
 void init_position(struct Board *pos, char *fen_str);
 char parse_castling_rights(char *castling_rights);
-
